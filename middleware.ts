1132b68a5c55db6a172b37f0d414913c9d771e25
karinahurzan <kgurzan@gmail.com>
2025-12-29 13:29:07 +0200

    update next js version

--- middleware.ts
+++ middleware.ts
@@ -6,8 +6,6 @@ const publicRoutes = ["/sign-in", "/sign-up"];
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
-  // В ТЗ зазвичай використовується "session-token" або "accessToken"
-  // Перевір назву куки в DevTools -> Application -> Cookies
   const accessToken = request.cookies.get("accessToken")?.value;
 
   const isPublicRoute = publicRoutes.some((route) =>
@@ -17,15 +15,11 @@ export async function middleware(request: NextRequest) {
     pathname.startsWith(route)
   );
 
-  // 1. Якщо користувач НЕ авторизований і йде на приватний роут
   if (isPrivateRoute && !accessToken) {
     const redirectUrl = new URL("/sign-in", request.url);
-    // Можна додати параметр, щоб після логіну повернути користувача сюди
-    // redirectUrl.searchParams.set("callbackUrl", pathname);
     return NextResponse.redirect(redirectUrl);
   }
 
-  // 2. Якщо користувач АВТОРИЗОВАНИЙ і йде на сторінки логіну/реєстрації
   if (isPublicRoute && accessToken) {
     return NextResponse.redirect(new URL("/profile", request.url));
   }
